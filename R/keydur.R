74d0d18daaf94e48279db0516e2cad05a9f1cb64
wkapga <wkapga@rcm.at>
2017-10-11 10:42:19 +0200

    added some doc

--- R/keydur.R
+++ R/keydur.R
@@ -1,32 +1,39 @@
 #' keyrate duration function
-#' 
+#'
 #' Calculates keyrate duration of a bond
-#' 
-#' 
-#' inputs:
-#' 
+#'
+#' @author wkapga
+#'
+#' @param keyrates list of keyrates
+#'
+#' @param ttm time until maturity in years
+#' @param coupon in percent
+#' @param yield in percent
+#' @param freq frequency of coupon payments
+#'
+#' @return tibble of key rate durations
 
-keydur <- function(ttm,coupon,yield,freq,keyrates,targetdur) {
+keydur <- function(keyrates,ttm,coupon,yield,freq,targetdur= NULL) {
   # function to calculate vector of key rate duarations for given keyrates
   # coupon, yield in percent, target duration is optional
   keyrates_expanded <- c(0, keyrates , tail(keyrates,1)*100 ) # add keyrate at 0 and far in future
   tt <- seq((ttm*freq - floor(ttm*freq))/freq, ttm, by = 1/freq ) # vector of occurence of cashflow in time
   cf <- rep(coupon/freq/100, length(tt)) + c( rep(0, length(tt)-1), 1) # cash flows
-  
+
   dcf <- cf * (1+yield/100)^(-tt) # discounted cash flows
   vec <- dcf * tt / sum(dcf) # duration per cash flow, total duration would be sum(vec)
-  
+
   kw <- map_dfc(tt, ~ as.tibble(wg(keyrates_expanded,.x))) # get matrix of weights
   k <- as.matrix(kw) %*% as.matrix(vec) %>% as.tibble %>%  flatten_dbl() # matrix multiplication
-  
+
   # add keyrate at zero to first keyrate and add keyrate far in future to last keyrate
   k <- c(sum(head(k,2)), k[3:(length(k)-2)], sum(tail(k,2)) )
-  
+
   # use stated duration if given
-  if ( ! missing(targetdur) ) { k <- k * targetdur/sum(k)  }
-  
+  if ( ! is.null(targetdur) ) { k <- k * targetdur/sum(k)  }
+
   return(tibble( kr = keyrates, val=k))
-} 
+}
 
 
 wg <- function (keyrates,tt) {
@@ -36,7 +43,7 @@ wg <- function (keyrates,tt) {
   #spr <- keyrates[i] - keyrates[i-1]
   # list of zeros length of keyrates
   w <- map_dbl(keyrates, ~ 0)
-  
+
   w <- modify_at(w,i, ~.x + (tt-keyrates[i-1] )/ (keyrates[i] - keyrates[i-1]) )
   w <- modify_at(w,i-1, ~.x + (keyrates[i]-tt )/ (keyrates[i] - keyrates[i-1]) )
   return(unlist(w))
