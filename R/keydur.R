f5d83b0920ca3c9aa992501531c21e081174697b
wkapga <wkapga@rcm.at>
2017-10-11 15:21:35 +0200

    better documentation of keydur code
    simplified wg function (without "purrr")

--- R/keydur.R
+++ R/keydur.R
@@ -25,17 +25,21 @@
 #' }
 
 keydur <- function(keyrates,ttm,coupon,yield,freq,targetdur= NULL) {
-  # function to calculate vector of key rate duarations for given keyrates
-  # coupon, yield in percent, target duration is optional
-  keyrates_expanded <- c(0, keyrates , tail(keyrates,1)*100 ) # add keyrate at 0 and far in future
-  tt <- seq((ttm*freq - floor(ttm*freq))/freq, ttm, by = 1/freq ) # vector of occurence of cashflow in time
-  cf <- rep(coupon/freq/100, length(tt)) + c( rep(0, length(tt)-1), 1) # cash flows
 
-  dcf <- cf * (1+yield/100)^(-tt) # discounted cash flows
-  vec <- dcf * tt / sum(dcf) # duration per cash flow, total duration would be sum(vec)
-
-  kw <- map_dfc(tt, ~ as.tibble(wg(keyrates_expanded,.x))) # get matrix of weights
-  k <- as.matrix(kw) %*% as.matrix(vec) %>% as.tibble %>%  flatten_dbl() # matrix multiplication
+  # add keyrate at 0 and far in future
+  keyrates_expanded <- c(0, keyrates , (tail(keyrates,1)+10) *100 )
+  # list of occurence of cashflow in time
+  tt <- seq((ttm*freq - floor(ttm*freq))/freq, ttm, by = 1/freq )
+  # list of value of cash flows
+  cf <- rep(coupon/freq/100, length(tt)) + c( rep(0, length(tt)-1), 1)
+  # discounted cash flows
+  dcf <- cf * ( 1 + yield / 100 ) ^ (-tt)
+  # duration per cash flow, total duration would be sum(vec)
+  vec <- dcf * tt / sum(dcf)
+  # get matrix of weights
+  kw <- map_dfc(tt, ~ as.tibble(wg(keyrates_expanded,.x)))
+  # matrix multiplication
+  k <- as.matrix(kw) %*% as.matrix(vec) %>% as.tibble %>%  flatten_dbl()
 
   # add keyrate at zero to first keyrate and add keyrate far in future to last keyrate
   k <- c(sum(head(k,2)), k[3:(length(k)-2)], sum(tail(k,2)) )
@@ -55,22 +59,20 @@ keydur <- function(keyrates,ttm,coupon,yield,freq,targetdur= NULL) {
 #' @return a list of weights with length of keyrates, sum = 1
 #' @export
 #'
-#' @importFrom purrr modify_at map_dbl detect_index
-#'
 #' @examples
 #'\dontrun{
 #' wg(c(2,5,10),6.2)
 #' }
 wg <- function (keyrates,tt) {
-  # helper function for key rate duration
-  # find index of first keyrate > tt
-  i <- keyrates %>% detect_index(~ .x > tt)
-  #spr <- keyrates[i] - keyrates[i-1]
   # list of zeros length of keyrates
-  w <- map_dbl(keyrates, ~ 0)
+  w <- keyrates * 0
 
-  w <- modify_at(w,i, ~.x + (tt-keyrates[i-1] )/ (keyrates[i] - keyrates[i-1]) )
-  w <- modify_at(w,i-1, ~.x + (keyrates[i]-tt )/ (keyrates[i] - keyrates[i-1]) )
+  # find index of first keyrate > tt
+  i <-  min(which(keyrates > tt))
+
+  #assign percentages
+  w[i] <- (tt-keyrates[i-1] )/ (keyrates[i] - keyrates[i-1])
+  w[i-1] <- 1 - w[i]
   return(unlist(w))
 }
 
